5dc36fef41c9b2240f27d1779348d0f1f589d205
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-26 18:05:34 +0600

    Create Printing Concatenated string using String formatting.cs

---
+++ Printing Concatenated string using String formatting.cs
@@ -0,0 +1,14 @@
+using System;
+ 
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			int firstNumber = 5, secondNumber = 10, result;
+			result = firstNumber + secondNumber;
+			Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, result);
+		}
+	}
+}
