0f39f047d6c130f0a46abd7bd67075f95018cf0b
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-26 18:01:34 +0600

    Create Printing Concatenated String using + operator.cs

---
+++ Printing Concatenated String using + operator.cs
@@ -0,0 +1,14 @@
+using System;
+ 
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			int val = 55;
+			Console.WriteLine("Hello " + "World");
+			Console.WriteLine("Value = " + val);
+		}
+	}
+}
